671a0ac1a0629663989d145a79842fcf0804383f
David <ga97vof@mytum.de>
2018-02-22 17:34:22 +0100

    added parameters

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import dicom
 import glob
 import os
+import sys
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
@@ -8,8 +10,20 @@ from scipy.misc import imresize
 
 from models import ImageSuperResolutionModel
 
+import argparse
 
-size = 64
+parser = argparse.ArgumentParser(description='Denoise CT Reconstructions')
+
+parser.add_argument('-b', action='store', dest='batch_size', default=128, type=int, help='Size of a batch')
+parser.add_argument('-s', action='store', dest='size', default=128, type=int, help='Width and height of image')
+parser.add_argument('-n', action='store', dest='noise_factor', default=0.2, type=float, help='Float between 0 and 1')
+parser.add_argument('-e', action='store', dest='nb_epochs', default=100, type=float, help='Number of epochs')
+
+args = parser.parse_args()
+batch_size = args.batch_size
+size = args.size
+noise_factor = args.noise_factor
+nb_epochs = args.nb_epochs
 
 
 def load_data():
@@ -35,6 +49,9 @@ else:
 
     np.save("./data/CPTAC-CM/data{}x{}.npy".format(size, size), x_data)
 
+if x_data.shape[0] == 0:
+    print("No data found, exeting!!")
+    sys.exit(0)
 
 x_data = np.asarray(x_data).astype('float32') / 255.
 
@@ -44,10 +61,8 @@ x_test = np.reshape(x_test, (len(x_test), size, size, 1))
 print("Training data:", x_train.shape)
 print("Test data:", x_test.shape)
 
-noise_factor = 0.
 x_train_noisy = x_train + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_train.shape)
 x_test_noisy = x_test + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_test.shape)
-
 
 x_train_noisy = np.clip(x_train_noisy, 0., 1.)
 x_test_noisy = np.clip(x_test_noisy, 0., 1.)
@@ -68,16 +83,16 @@ autoencoder.create_model(height=size, width=size, channels=1)
 
 autoencoder.model.summary()
 
-autoencoder.fit(x_train_noisy, x_train, )
+autoencoder.fit(x_train_noisy, x_train, batch_size=batch_size, nb_epochs=nb_epochs, save_history=True)
 
-decoded_imgs = autoencoder.predict(x_test)
+decoded_imgs = autoencoder.predict(x_test, batch_size=batch_size)
 
 n = 10
 plt.figure(figsize=(20, 4))
 indices = np.random.randint(0, len(x_test_noisy), size=n)
 for i, ind in enumerate(indices):
     # display original
-    ax = plt.subplot(2, n, i+1)
+    ax = plt.subplot(2, n, i + 1)
     plt.imshow(x_test_noisy[ind].reshape(size, size))
     plt.gray()
     ax.get_xaxis().set_visible(False)
