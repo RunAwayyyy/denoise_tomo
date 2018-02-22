932c589241a41c219de8fa573337b22a5ca21889
David <ga97vof@mytum.de>
2018-02-22 18:00:57 +0100

    refactored data loading

--- test.py
+++ test.py
@@ -5,10 +5,11 @@ import sys
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
 from scipy.misc import imresize
 
 from models import ImageSuperResolutionModel
+from data_loader import load
+
 
 import argparse
 
@@ -29,47 +30,7 @@ noise_factor = args.noise_factor
 nb_epochs = args.nb_epochs
 load_weights = args.load_weights
 
-
-def load_data():
-    dataset = sorted(glob.glob("./data/CPTAC-CM/DOI/C3L-00629/**/*.dcm", recursive=True))
-
-    x_data = []
-    for file in dataset:
-        ds = dicom.read_file(file)
-        shape = (int(ds.Rows), int(ds.Columns))
-        spacing = (float(ds.SliceThickness), float(ds.PixelSpacing[0]), float(ds.PixelSpacing[1]))
-        # if shape == (512, 512):
-        array = np.zeros(shape, dtype=ds.pixel_array.dtype)
-        array[:, :] = ds.pixel_array / 255.
-        x_data.append(imresize(array, (size, size, 1)))
-    return x_data
-
-
-if os.path.exists("./data/CPTAC-CM/data{}x{}.npy".format(size, size)):
-    x_data = np.load("./data/CPTAC-CM/data{}x{}.npy".format(size, size))
-else:
-    print("Load data")
-    x_data = load_data()
-
-    np.save("./data/CPTAC-CM/data{}x{}.npy".format(size, size), x_data)
-
-if x_data.shape[0] == 0:
-    print("No data found, exeting!!")
-    sys.exit(0)
-
-x_data = np.asarray(x_data).astype('float32') / 255.
-
-x_train, x_test = train_test_split(x_data, test_size=0.25)
-x_train = np.reshape(x_train, (len(x_train), size, size, 1))
-x_test = np.reshape(x_test, (len(x_test), size, size, 1))
-print("Training data:", x_train.shape)
-print("Test data:", x_test.shape)
-
-x_train_noisy = x_train + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_train.shape)
-x_test_noisy = x_test + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_test.shape)
-
-x_train_noisy = np.clip(x_train_noisy, 0., 1.)
-x_test_noisy = np.clip(x_test_noisy, 0., 1.)
+x_train_noisy, x_test_noisy, y_train, y_test = load(size, noise_factor)
 
 """n = 10
 plt.figure(figsize=(20, 2))
@@ -87,9 +48,9 @@ autoencoder.create_model(height=size, width=size, channels=1, load_weights=load_
 
 autoencoder.model.summary()
 
-autoencoder.fit(x_train_noisy, x_train, batch_size=batch_size, nb_epochs=nb_epochs, save_history=True)
+autoencoder.fit(x_train_noisy, y_train, batch_size=batch_size, nb_epochs=nb_epochs, save_history=True)
 
-decoded_imgs = autoencoder.predict(x_test, batch_size=batch_size)
+decoded_imgs = autoencoder.predict(y_test, batch_size=batch_size)
 
 n = 10
 plt.figure(figsize=(20, 4))
